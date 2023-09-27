c30d493d46718f83a3545da1fa4e89eb022c202c
Akash Goswami <imakash93@users.noreply.github.com>
2023-09-27 12:41:29 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,32 @@
 const outputDiv = document.getElementById('output');
+const runPythonButton = document.getElementById('runPythonButton');
 
-// Initialize Pyodide
-languagePluginLoader.then(() => {
-    return pyodide.loadPackage(['numpy']);
-}).then(() => {
-    // Fetch the Python script from an external file
-    fetch('py.py')
-        .then((response) => response.text())
-        .then((pythonCode) => {
-            // Run Python code
-            pyodide.runPython(pythonCode).then(output => {
-                // Display the Python output
-                outputDiv.textContent = `Python Output: ${output}`;
-            }).catch(error => {
-                console.error('Error running Python:', error);
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching Python script:', error);
-        });
+runPythonButton.addEventListener('click', () => {
+
+    const PYODIDE_BASE_URL = "https://cdn.jsdelivr.net/pyodide/v0.18.0/full"
+
+loadPyodide({ indexURL: PYODIDE_BASE_URL }).then((pyodide) => {
+  globalThis.pyodide = pyodide      // you might also want to store pyodide globally so 
+                                    // so you can access anywhere in the scope
+  pyodide.loadPackage(['numpy']).then(() => {
+    pyodide.runPython(`   
+    # My python code here
+  `);
+
+  fetch('py.py') // Replace with the actual path
+  .then((response) => response.text())
+  .then((pythonCode) => {
+      // Run Python code
+      pyodide.runPython(pythonCode).then(output => {
+          // Display the Python output
+          outputDiv.textContent = `Python Output: ${output}`;
+      }).catch(error => {
+          console.error('Error running Python:', error);
+      });
+  })
+  .catch(error => {
+      console.error('Error fetching Python script:', error);
+  });
+});});
 });
+
